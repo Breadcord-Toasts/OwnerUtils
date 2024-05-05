95e9779b889b8bc4dd7e4a0b66de66ae53c3715a
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2024-05-05 15:36:14 +0200

    Rewrite sync command

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ import textwrap
 from contextlib import redirect_stdout, redirect_stderr
 from pathlib import Path
 from pprint import pprint, pp
-from typing import Literal, Any
+from typing import Any
 
 import aiohttp
 import discord.abc
@@ -200,77 +200,114 @@ class OwnerUtils(breadcord.module.ModuleCog):
         await ctx.reply("Stopping bot")
         await self.bot.close()
 
-    # The docstring is only meant to be used by the help command
-    # noinspection PyIncorrectDocstring
     @commands.command()
     @commands.guild_only()
     @commands.is_owner()
     async def sync(
         self,
         ctx: commands.Context,
-        guilds: commands.Greedy[discord.Guild],
-        scope: str | None = None,
-        mode: str | None = None
+        guilds: commands.Greedy[discord.Guild] = commands.parameter(
+            description="IDs of the guilds to sync commands to."
+        ),
+        scope: str | None = commands.parameter(
+            description="\n".join((
+                'The scope in which to sync commands. ',
+                'Can be "global" or "local" (or an allowed synonym). ',
+                'If guilds are provided, only a local scope is allowed, and will be appended to the guilds to sync to.',
+            )),
+            default=None
+        ),
+        mode: str | None = commands.parameter(
+            description="\n".join((
+                'The mode of the command. Can be "clear", "copy", or "sync" (or an allowed synonym). ',
+                'Defaults to syncing.'
+            )),
+            default=None,
+        ),
+        clear_command_type: str | None = commands.parameter(
+            description="\n".join((
+                'If the mode is set to clear, this is the type of app command to clear. ',
+                'Can be "slash", "user" or "message" specifying if slash commands or context menus should be cleared. ',
+                'Defaults to clearing all app command types.',
+            )),
+            default=None
+        )
     ) -> None:
-        """Syncs application commands
+        """Syncs application commands."""
+        global_scopes = ["all", "global", "globally"]
+        local_scopes = ["local", "locally", "here"]
+        valid_scopes = global_scopes + local_scopes
+        valid_modes = ["clear", "copy", "sync"]
+        valid_clear_command_types = {
+            "chat_input": "chat_input",
+            "slash": "chat_input",
+            "user": "user",
+            "ctx_user": "user",
+            "message": "message",
+            "ctx_message": "message",
+        }
 
-        Parameters
-        -----------
-        guilds:
-            The IDs of guilds to sync to.
-        scope:
-            Alternative to specifying guild IDs.
-            `all` or `global` syncs in all guilds
-            `local` or `here` syncs in the current guild
-        mode:
-            Alternative sync modes.
-            `clear` clears the commands and then syncs.
-            `copy` copies the global commands into the guild as guild-specific commands.
-        """
-
-        guilds: list[discord.Guild] | Literal["all"] = guilds
         scope = scope.lower() if scope else None
-        mode: Literal["clear", "copy"] | None = mode.lower() if mode else None
+        mode = mode.lower() if mode else None
 
-        if guilds:
-            if mode:
-                raise commands.TooManyArguments(f"Too many arguments passed to {ctx.command.qualified_name}")
+        if guilds and scope in global_scopes:
+            raise commands.BadArgument(f"Can not use a global scope when guilds are provided")
+
+        if mode is None and scope is not None and scope not in valid_scopes:
             mode = scope
-        elif scope in ["all", "global", "globally"]:
-            guilds = "all"
-        elif scope in ["local", "locally", "here"]:
-            guilds = [ctx.guild]
+            scope = None
 
-        if mode is None:
-            if guilds:
-                response = await ctx.reply(f"Syncing commands in {len(guilds)} guild(s)..")
-                for guild in guilds:
-                    await ctx.bot.tree.sync(guild=guild)
-                await response.edit(content=f"Synced commands in {len(guilds)} guild(s)")
-            else:
-                response = await ctx.reply("Syncing commands in all guilds..")
+        if scope is not None and scope not in valid_scopes:
+            raise commands.BadArgument(f"Invalid scope: {scope}")
+        mode = mode or "sync"
+        if mode is not None and mode not in valid_modes:
+            raise commands.BadArgument(f"Invalid mode: {mode}")
+        if clear_command_type is not None:
+            if mode != "clear":
+                raise commands.BadArgument(f"Can not clear command types when not in clear mode")
+            if clear_command_type not in valid_clear_command_types:
+                raise commands.BadArgument(f"Invalid clear command type: {clear_command_type}")
+            clear_command_type = valid_clear_command_types[clear_command_type]
+
+        if scope in local_scopes:
+            guilds.append(ctx.guild)
+
+        targets = list(set(guilds)) or None
+        target_message = "all guilds"
+        if targets:
+            target_message = f"{len(targets)} guilds" if len(targets) > 1 else "1 guild"
+
+        del scope, guilds
+
+        if mode == "sync":
+            response = await ctx.reply(f"Syncing commands in {target_message}..")
+            if not targets:
                 await ctx.bot.tree.sync()
-                await response.edit(content="Synced commands in all guilds")
-            return
-        elif mode == "clear":
-            if guilds == "all":
-                response = await ctx.reply("Clearing commands in all guilds..")
-                ctx.bot.tree.clear_commands()
-                await ctx.bot.tree.sync()
-                await response.edit(content="Cleared commands in all guilds")
             else:
-                response = await ctx.reply(f"Clearing commands in {len(guilds)} guild(s)..")
-                for guild in guilds:
-                    ctx.bot.tree.clear_commands(guild=guild)
+                for guild in targets:
                     await ctx.bot.tree.sync(guild=guild)
-                await response.edit(content=f"Cleared commands in {len(guilds)} guild(s)")
+            await response.edit(content=f"Synced commands in {target_message}")
             return
-        elif mode == "copy" and guilds != "all":
-            response = await ctx.reply(f"Copying global commands to {len(guilds)} guild(s)..")
-            for guild in guilds:
+
+        if targets is None:
+            raise commands.BadArgument("Clearing or copying to all guilds is not allowed.")
+        elif not targets:
+            raise commands.BadArgument("No guilds to target.")
+
+        if mode == "clear":
+            response = await ctx.reply(f"Clearing commands in {target_message}..")
+            for guild in targets:
+                ctx.bot.tree.clear_commands(guild=guild)
+                await ctx.bot.tree.sync(guild=guild)
+            await response.edit(content=f"Cleared commands in {target_message}")
+            return
+
+        if mode == "copy":
+            response = await ctx.reply(f"Copying global commands to {target_message}..")
+            for guild in targets:
                 ctx.bot.tree.copy_global_to(guild=guild)
                 await ctx.bot.tree.sync(guild=guild)
-            await response.edit(content=f"Copied global commands to {len(guilds)} guild(s)")
+            await response.edit(content=f"Copied global commands to {target_message}")
             return
 
         raise commands.BadArgument()
@@ -278,7 +315,7 @@ class OwnerUtils(breadcord.module.ModuleCog):
     @sync.error
     async def sync_error(self, ctx: commands.Context, error: Exception) -> None:
         if isinstance(error, commands.BadArgument):
-            await ctx.reply("Invalid arguments were passed")
+            await ctx.reply(str(error))
             return
         raise error
 
